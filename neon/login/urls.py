37ac2c3bd0d80043f56a6c95f5c221a14e26684f
Rahul Sindhav <rahulkumarsindhav@gmail.com>
2020-03-28 18:54:30 +0530

    basic templates for login and home are ready

--- neon/login/urls.py
+++ neon/login/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('auth/', views.auth_view),
+    path('loggedin/', views.loggedin),
+    path('invalidlogin/', views.invalidlogin),
+    path('logout/', views.logout),
 ]
--- neon/student/views.py
+++ neon/student/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.template.context_processors import csrf
+from django.contrib.auth import *
+from django.contrib.auth.models import User
+
+from login.models import Student
 # Create your views here.
 
+
+@login_required(login_url='/login/')
 def index(request):
-    return HttpResponse("Hello World from Django!")
+    c = {}
+    c.update(csrf(request))
+    if request.user.is_authenticated:
+        id = request.user.id
+        user = User.objects.get(id=id)
+        if user is not None:
+            username = user.username
+            student = Student.objects.filter(student_id = username)
+            count = student.count()
+            if int(count) > 0:
+                c['student'] = student[0]
+            return render(request,'home.html',c)
+        else:
+            return HttpResponseRedirect('/login/invalidlogin')
