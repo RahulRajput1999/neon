82f0db3485d13f184a9d07d72fb8fb0f12c766c9
Rahul Sindhav <rahulkumarsindhav@gmail.com>
2020-04-22 21:21:23 +0530

    production wsgi

--- neon/neon/wsgi.py
+++ neon/neon/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neon.neon.settings')
 
 application = get_wsgi_application()
